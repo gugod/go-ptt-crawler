d55539925c2f8f093cd3755b3d5b8880865eaff7
Kang-min Liu <gugod@gugod.org>
2015-10-01 00:18:17 +0200

    finish the article converter

--- convert-articles-html.go
+++ convert-articles-html.go
@@ -4,13 +4,83 @@ import (
 	"os"
 	"log"
 	"strings"
+	"encoding/json"
 	"path/filepath"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
+type PushLine struct {
+	Tag        string `json:"tag"`
+	UserId     string `json:"userid"`
+	Content    string `json:"content"`
+	IpDateTime string `json:"ipdatetime"`	
+}
+
+type MetaLine []string
+
+type PttArticle struct {
+	Body string     `json:"body"`
+	Meta []MetaLine `json:"meta"`
+	Push []PushLine `json:"push"`
+}
+
 func visit(path string, f os.FileInfo, err error) error {
+	var output_path string
+	var e error
+	var fr *os.File
+	var fw *os.File
+	var doc *goquery.Document
+	var body string
+	var pushes []PushLine
+	var meta []MetaLine
+
 	if ! strings.HasSuffix(path, ".html") { return nil }
 
-	log.Printf("Visited: %s\n", path)		
+	if fr, e = os.Open(path); e != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	if doc, e = goquery.NewDocumentFromReader(fr); e != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	main_content_dom := doc.Find("#main-content")
+	main_content_dom.Find(".article-metaline").Each(
+		func(i int, s *goquery.Selection) {
+			var p = MetaLine{ s.Find(".article-meta-tag").Text(), s.Find(".article-meta-value").Text() }
+			meta = append(meta, p)
+		});
+
+	main_content_dom.Find("div.push").Each(
+		func(i int, s *goquery.Selection) {
+			pushes = append(pushes, PushLine{
+				Tag: s.Find(".push-tag").Text(),
+				UserId: s.Find(".push-userid").Text(),
+				Content: s.Find(".push-content").Text(),
+				IpDateTime: s.Find(".push-ipdatetime").Text(),
+			})
+		});
+
+	body = doc.Find("#main-content").Text()
+
+	var ptt_article = PttArticle{Body: body, Meta: meta, Push: pushes}
+	
+	var b []byte
+	if b, e = json.MarshalIndent(ptt_article, "", "   "); e != nil {
+		log.Fatal(err)
+		return nil		
+	}
+
+	output_path = strings.TrimSuffix(path, ".html") + ".json"
+	if fw, e = os.Create(output_path); e != nil {
+		log.Fatal(e)
+		return nil
+	}
+	log.Printf("%s", output_path)
+	fw.Write(b)
 	return nil
 } 
 
